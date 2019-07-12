a074c1af3a1a5e96ffc780c13f646a761514524d
Angelo G. Del Regno <kholk11@gmail.com>
2019-07-12 16:31:11 +0200

    drm: msm: sde_hw_interrupts: Do not try tear irqs on SDM845 target
    This target has the MDP tear IRQ in the MDP intf IRQ-space.
    
    Signed-off-by: Artem Labazov <123321artyom@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_hw_interrupts.c
+++ drivers/gpu/drm/msm/sde/sde_hw_interrupts.c
@@ -18,6 +18,10 @@
 #include "sde_hw_util.h"
 #include "sde_hw_mdss.h"
 
+#if defined(CONFIG_ARCH_SDM845)
+#define TARGET_HAS_TEAR_IRQS_IN_MDP
+#endif
+
 /**
  * Register offsets in MDSS register file for the interrupt registers
  * w.r.t. to the MDSS base
@@ -277,6 +281,7 @@ static const struct sde_intr_reg sde_intr_set[] = {
 		MDP_AD4_1_OFF + MDP_AD4_INTR_EN_OFF,
 		MDP_AD4_1_OFF + MDP_AD4_INTR_STATUS_OFF,
 	},
+#ifndef TARGET_HAS_TEAR_IRQS_IN_MDP
 	{
 		MDP_INTF_TEAR_INTF_1_IRQ_OFF + MDP_INTF_TEAR_INTR_CLEAR_OFF,
 		MDP_INTF_TEAR_INTF_1_IRQ_OFF + MDP_INTF_TEAR_INTR_EN_OFF,
@@ -287,6 +292,7 @@ static const struct sde_intr_reg sde_intr_set[] = {
 		MDP_INTF_TEAR_INTF_2_IRQ_OFF + MDP_INTF_TEAR_INTR_EN_OFF,
 		MDP_INTF_TEAR_INTF_2_IRQ_OFF + MDP_INTF_TEAR_INTR_STATUS_OFF,
 	}
+#endif
 };
 
 /**
